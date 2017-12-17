93093c6edea12a4da79b65eb41f0642b58f23296
LongWinter <ye@yuans-mbp.gateway.mts.net>
2017-12-16 22:20:56 -0600

    print the first spam and ham message in each directory

--- SpamFilter.java
+++ SpamFilter.java
@@ -3,11 +3,14 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.ArrayList;
 import java.util.regex.*;
+import java.text.DecimalFormat;
 
 public class SpamFilter{
     static HashMap<String, Word> dictionary = new HashMap<String, Word>();
     static int totalSpam = 0;
     static int totalHam = 0;
+    static int totalWordInSpam = 0;
+    static int totalWordInHam = 0;
 
     static int totalHamInTesting = 0;
     static int totalSpamInTesting = 0;
@@ -29,9 +32,9 @@ public class SpamFilter{
 
         System.out.println("Total Spam in training/spam: "+totalSpam);
         System.out.println("Total Ham in training/ham: "+totalHam);
-        removeNoise1();
+        //removeNoise1();
         processWordProbability();
-        removeNoise2();
+        //removeNoise2();
         //printFrequencyTable();
 
         try{
@@ -43,14 +46,18 @@ public class SpamFilter{
 
         System.out.println("\n\nTotal Ham in testing: "+ totalHamInTesting);
         System.out.println("Total predicted Spam in Testing/Ham: "+ predictSpamInTestingHam);
+        System.out.println("Probability: "+formatPercent( 1-(double)predictHamInTestingHam/totalHamInTesting, 11));
         System.out.println("Total predicted Ham in Testing/Ham: "+ predictHamInTestingHam);
-        System.out.println("Total number of files cannot predict in Testing/Ham: "+cantPredictInTestingHam);
+        System.out.println("Probability: "+formatPercent( (double)predictHamInTestingHam/totalHamInTesting, 8));
+
         System.out.println();
 
         System.out.println("\n\nTotal Spam in testing: "+ totalSpamInTesting);
         System.out.println("Total predicted Spam in Testing/Spam: "+ predictSpamInTestingSpam);
+        System.out.println("Probability: "+formatPercent((double)predictSpamInTestingSpam/totalSpamInTesting, 8) );
         System.out.println("Total predicted Ham in Testing/Spam: "+ predictHamInTestingSpam);
-        System.out.println("Total number of files cannot predict in Testing/Spam: "+cantPredictInTestingSpam);
+        System.out.println("Probability: "+formatPercent( (double)predictHamInTestingSpam/totalSpamInTesting, 8) );
+
         System.out.println();
 
         
@@ -72,12 +79,12 @@ public class SpamFilter{
                     findContent = true;
                 }else{
                     if(findContent == true){
-                        String[] splitted = line.split("[\\s\\.\\!\\\"\\%\\$\\*\\+\\&\\,\\?\\/\\<\\>\\#\\-\\)\\:\\(\\)\\~\\{\\}\\;\\[\\]]+");
-                        //String[] splitted = line.split("\\~\\!\\@\\#\\$\\%\\^\\&\\*\\(\\)\\_\\-\\+\\=\\,\\<\\.\\>\\?\\/\\[\\{\\}\\}\\;\\:\\'\\|\\\"");
+                        //String[] splitted = line.split("[\\s\\.\\!\\\"\\%\\$\\*\\+\\&\\,\\?\\/\\<\\>\\#\\-\\)\\:\\(\\)\\~\\{\\}\\;\\[\\]]+");
+                        String[] splitted = line.split(" ");
                         for (String a : splitted){
                             a = a.trim();
                             if(!a.isEmpty()){
-                                //totalHam++;
+                                totalWordInHam++;
                                 if(dictionary.containsKey(a)){
                                     Word word = dictionary.get(a);
                                     word.ham();
@@ -115,12 +122,12 @@ public class SpamFilter{
                     findContent = true;
                 }else{
                     if(findContent == true){
-                        String[] splitted = line.split("[\\s\\.\\!\\\"\\%\\$\\*\\+\\&\\,\\?\\/\\<\\>\\#\\-\\)\\:\\(\\)\\~\\{\\}\\;\\[\\]]+");
-                        //String[] splitted = line.split("\\~\\!\\@\\#\\$\\%\\^\\&\\*\\(\\)\\_\\-\\+\\=\\,\\<\\.\\>\\?\\/\\[\\{\\}\\}\\;\\:\\'\\|\\\"");
+                        //String[] splitted = line.split("[\\s\\.\\!\\\"\\%\\$\\*\\+\\&\\,\\?\\/\\<\\>\\#\\-\\)\\:\\(\\)\\~\\{\\}\\;\\[\\]]+");
+                        String[] splitted = line.split(" ");
                         for (String a : splitted){
                             a = a.trim();
                             if(!a.isEmpty()){
-                                //totalSpam++;
+                                totalWordInSpam++;
                                 if(dictionary.containsKey(a)){
                                     Word word = dictionary.get(a);
                                     word.spam();
@@ -169,8 +176,10 @@ public class SpamFilter{
     // calculate the conditional probability based on the paper
     public static void processWordProbability(){
         int k = dictionary.size();
-
+        System.out.println("total words in Spams: "+totalWordInSpam);
+        System.out.println("total words in Hams: "+totalWordInHam);
         for(Map.Entry<String, Word> entry : dictionary.entrySet()){
+            //entry.getValue().calculateRate(totalWordInSpam, totalWordInHam, k);
             entry.getValue().calculateRate(totalSpam, totalHam, k);
         }
     }
@@ -213,6 +222,10 @@ public class SpamFilter{
     //this is the method to test the model with Hams
     public static void processTestDataHam() throws IOException{
         File dir = new File("./testing/ham");
+        ArrayList<String> sentence_spam = new ArrayList<String>();
+        int counter_spam = 0;
+        ArrayList<String> sentence_ham = new ArrayList<String>();
+        int counter_ham = 0;
 
         for(File f : dir.listFiles()){
             // read one file
@@ -224,12 +237,20 @@ public class SpamFilter{
 
             while(line != null){
                 line.trim();
+
+                if (counter_spam < 1){
+                    sentence_spam.add(line);
+                }
+                if (counter_ham < 1){
+                    sentence_ham.add(line);
+                }
+
                 if (line.isEmpty()){
                     findContent = true;
                 }else{
                     if(findContent == true){
-                        String[] splitted = line.split("[\\s\\.\\!\\\"\\%\\$\\*\\+\\&\\,\\?\\/\\<\\>\\#\\-\\)\\:\\(\\)\\~\\{\\}\\;\\[\\]]+");
-                        //String[] splitted = line.split("\\~\\!\\@\\#\\$\\%\\^\\&\\*\\(\\)\\_\\-\\+\\=\\,\\<\\.\\>\\?\\/\\[\\{\\}\\}\\;\\:\\'\\|\\\"");
+                        //String[] splitted = line.split("[\\s\\.\\!\\\"\\%\\$\\*\\+\\&\\,\\?\\/\\<\\>\\#\\-\\)\\:\\(\\)\\~\\{\\}\\;\\[\\]]+");
+                        String[] splitted = line.split(" ");
                         for (String a : splitted){
                             a = a.trim();
                             if(!a.isEmpty()){
@@ -243,30 +264,53 @@ public class SpamFilter{
 
                 line = reader.readLine();
             }
-            if (wordsInEmail.size() > 0){
+            if (wordsInEmail.size() >= 0){
                 //p(S|E)/p(H|E)
                 double p = 1;
 
                 for(String s : wordsInEmail){
                     p = p * dictionary.get(s).getSpamRate() / dictionary.get(s).getHamRate();
                 }
-                p = p *totalSpam/totalSpam;
+                //p = p *totalSpam/totalSpam;
+                p = p * totalSpam/totalHam;
 
-                if (p >= 1){
+                if (p > 1){
                     predictSpamInTestingHam++;
-                }else{
+                    counter_spam++;
+                    //print the email content
+                    if(counter_spam == 1){
+                        System.out.println("The First Spam in testing/ham is: ");
+                        for (String s : sentence_spam){
+                            System.out.println(s);
+                        }
+                    }
+                }
+                else{
                     predictHamInTestingHam++;
+                    counter_ham++;
+                    if (counter_ham ==1){
+                        System.out.println("The First Ham in testing/ham is: ");
+                        for(String s: sentence_ham){
+                            System.out.println(s);
+                        }
+                    }
                 }
 
             }else{
                 cantPredictInTestingHam++;
             }
+            sentence_ham.clear();
+            sentence_spam.clear();
             reader.close();
         }
     }
 
     public static void processTestDataSpam() throws IOException{
         File dir = new File("./testing/spam");
+        ArrayList<String> sentence_spam = new ArrayList<String>();
+        int counter_spam = 0;
+        ArrayList<String> sentence_ham = new ArrayList<String>();
+        int counter_ham = 0;
 
         for(File f : dir.listFiles()){
             // read one file
@@ -278,12 +322,20 @@ public class SpamFilter{
 
             while(line != null){
                 line.trim();
+
+                if (counter_spam < 1){
+                    sentence_spam.add(line);
+                }
+                if (counter_ham < 1){
+                    sentence_ham.add(line);
+                }
+
                 if (line.isEmpty()){
                     findContent = true;
                 }else{
                     if(findContent == true){
-                        String[] splitted = line.split("[\\s\\.\\!\\\"\\%\\$\\*\\+\\&\\,\\?\\/\\<\\>\\#\\-\\)\\:\\(\\)\\~\\{\\}\\;\\[\\]]+");
-                        //String[] splitted = line.split("\\~\\!\\@\\#\\$\\%\\^\\&\\*\\(\\)\\_\\-\\+\\=\\,\\<\\.\\>\\?\\/\\[\\{\\}\\}\\;\\:\\'\\|\\\"");
+                        //String[] splitted = line.split("[\\s\\.\\!\\\"\\%\\$\\*\\+\\&\\,\\?\\/\\<\\>\\#\\-\\)\\:\\(\\)\\~\\{\\}\\;\\[\\]]+");
+                        String[] splitted = line.split(" ");
                         for (String a : splitted){
                             a = a.trim();
                             if(!a.isEmpty()){
@@ -297,26 +349,52 @@ public class SpamFilter{
 
                 line = reader.readLine();
             }
-            if (wordsInEmail.size() > 0){
+            if (wordsInEmail.size() >= 0){
                 //p(S|E)/p(H|E)
                 double p = 1;
 
                 for(String s : wordsInEmail){
                     p = p * dictionary.get(s).getSpamRate() / dictionary.get(s).getHamRate();
                 }
-                p = p *totalSpam/totalSpam;
+                p = p *totalSpam/totalHam;
 
-                if (p >= 1){
+                if (p > 1){
                     predictSpamInTestingSpam++;
+                    counter_spam++;
+                    //print the email content
+                    if(counter_spam == 1){
+                        System.out.println("The First Spam in testing/spam is: ");
+                        for (String s : sentence_spam){
+                            System.out.println(s);
+                        }
+                    }
+                    
                 }else{
                     predictHamInTestingSpam++;
+                    counter_ham++;
+                    if (counter_ham ==1){
+                        System.out.println("The First Ham in testing/spam is: ");
+                        for(String s: sentence_ham){
+                            System.out.println(s);
+                        }
+                    }
                 }
 
             }else{
                 cantPredictInTestingSpam++;
             }
+            sentence_spam.clear();
+            sentence_ham.clear();
             reader.close();
         }
     }
 
+    public static String formatPercent(double done, int digits) {
+        DecimalFormat percentFormat = new DecimalFormat("0.0%");
+        percentFormat.setDecimalSeparatorAlwaysShown(false);
+        percentFormat.setMinimumFractionDigits(digits);
+        percentFormat.setMaximumFractionDigits(digits);
+        return percentFormat.format(done);
+      }
 }
+
